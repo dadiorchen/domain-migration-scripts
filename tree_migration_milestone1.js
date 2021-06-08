c919970ce58e8c217e15bf157e615bb4317a669b
Arun Bakt <arunbakt@gmail.com>
2021-06-07 21:56:01 -0700

    refactor: use writeStream with pipes and fix tree_attributes bug

--- tree_migration_milestone1.js
+++ tree_migration_milestone1.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const { knex } = require("./database/knex");
-
-var ProgressBar = require("progress");
+const Writable = require('stream').Writable;
+const ws = Writable({ objectMode: true });
+const ProgressBar = require("progress");
 
 async function migrate() {
   try {
     const base_query_string =
-      "from public.trees t left join field_data.raw_capture r on t.uuid = r.id::text where t.approved=true and r.id is null";
-
-    const count = await knex.select(knex.raw(`count(*) ${base_query_string}`));
-    const query_stream = knex.raw(`select t.* ${base_query_string}`).stream();
-
+      "select t.* from public.trees t left join field_data.raw_capture r on t.uuid = r.id::text where t.approved=true and r.id is null";
+    const rowCountResult = await knex.select(knex.raw(`count(1) from (${base_query_string}) as src`));
+    console.log(`Migrating ${+rowCountResult[0].count} records`);
+    
     var bar = new ProgressBar("Migrating [:bar] :percent :etas", {
       width: 20,
-      total: +count[0].count,
+      total: +rowCountResult[0].count,
     });
 
     const attributes_formatter = (attributes) => {
@@ -22,6 +22,7 @@ async function migrate() {
       for (let entry of attributes) {
         attributes_in_json_format.entries.push(entry);
       }
+      return attributes_in_json_format;
     };
 
     const create_target_objects = (
@@ -55,6 +56,7 @@ async function migrate() {
         lon,
         updated_at: time_updated,
       });
+
       const raw_capture_to_insert = Object.freeze({
         ...base_target_object,
         field_user_photo_url: planter_photo_url,
@@ -71,9 +73,18 @@ async function migrate() {
       };
     };
 
-    for await (const tree_object of query_stream) {
+   
+    ws._write = async (tree_object, enc, next) => {
       const trx = await knex.transaction();
       try {
+        if (!tree_object.planter_identifier && tree_object.planter_id) {
+          const planter = await trx
+            .select()
+            .table("public.planter")
+            .where("id", tree_object.planter_id)
+            .first();
+          tree_object.planter_identifier = planter.email ?? planter.phone;
+        }
         const tree_attributes = await trx
           .select()
           .table("public.tree_attributes")
@@ -116,10 +127,14 @@ async function migrate() {
           console.log("Migration Complete");
         }
       } catch (e) {
+        console.log(`Error processing tree id ${tree_object.id} ${e}`);
         trx.rollback();
-        throw e;
       }
+      next();
     }
+    const query_stream = knex.raw(`${base_query_string}`).stream()
+    query_stream.pipe(ws);
+
   } catch (err) {
     console.log(err);
   }
