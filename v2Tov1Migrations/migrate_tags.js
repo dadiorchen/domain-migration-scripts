23273ef6b586296fe808d8a589c8b4bd300ae9c2
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2022-10-16 00:06:35 +0100

    fix: migrate tags

---
+++ v2Tov1Migrations/migrate_tags.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const ProgressBar = require('progress');
+const { Writable } = require('stream');
+
+const ws = Writable({ objectMode: true });
+const { knex } = require('../database/knex');
+const createTags = require('./helper/createTags.js');
+
+async function migrate() {
+  const base_query_string = `SELECT * FROM public.tag where active = true`;
+
+  const rowCountResult = await knex.select(
+    knex.raw(`count(1) from (${base_query_string}) as src`),
+  );
+  console.log(`Migrating ${+rowCountResult[0].count} records`);
+
+  const bar = new ProgressBar('Migrating [:bar] :percent :etas', {
+    width: 100,
+    total: +rowCountResult[0].count,
+  });
+
+  const trx = await knex.transaction();
+
+  ws._write = async (tag, enc, next) => {
+    try {
+      await createTags(tag, trx);
+
+      bar.tick();
+      if (bar.complete) {
+        await trx.commit();
+        console.log('Migration Complete');
+        process.exit();
+      }
+    } catch (e) {
+      console.log(e);
+      console.log(`Error processing tag id ${tag.id} ${e}`);
+      await trx.rollback();
+      process.exit(1);
+    }
+    next();
+  };
+
+  const query_stream = knex.raw(`${base_query_string}`).stream();
+  query_stream.pipe(ws);
+}
+
+migrate();
